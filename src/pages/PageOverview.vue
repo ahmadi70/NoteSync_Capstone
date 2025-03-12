885c1b29ee985f4c68338aedc04378ddb47cb57c
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-03-12 21:47:36 +0100

    update overview page to use storeNotes

--- src/pages/PageOverview.vue
+++ src/pages/PageOverview.vue
@@ -1,15 +1,23 @@
-<script setup></script>
+<script setup>
+import { useStoreNotes } from '@/stores/storeNotes'
+
+const storeNotes = useStoreNotes()
+</script>
 
 <template> 
   <MainSection>
     <div class="bg-white w-full flex flex-col gap-3 p-3">
       <div class="flex justify-between">
-        <div>Notes</div>
-        <div>Characters</div>
+        <div>Overview</div>
+        <div>Value</div>
       </div>
       <div class="flex justify-between">
-        <div>2</div>
-        <div>14</div>
+        <div>Number of Notes</div>
+        <div>{{ storeNotes.getNotes }}</div>
+      </div>
+      <div class="flex justify-between">
+        <div>Number of Character (all notes)</div>
+        <div>{{ storeNotes.getNotesCharacters }}</div>
       </div>
     </div>
   </MainSection>
