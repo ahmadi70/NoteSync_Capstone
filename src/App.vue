142f88617c06ad306ad5957d850e35b687b4879f
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-03-13 20:17:01 +0100

    load all notes when the app mounted

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,11 @@
-<script setup></script>
+<script setup>
+import { useStoreNotes } from '@/stores/storeNotes'
+
+const storeNotes = useStoreNotes()
+onMounted(() => {
+  storeNotes.getNotes()
+})
+</script>
 
 <template>
   <div>
