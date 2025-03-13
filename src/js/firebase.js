ab8c4083ecb204dbc217d97ef10f41d435436cb4
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-03-13 20:15:39 +0100

    install and configure firebase

---
+++ src/js/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCzoeAQ5mgq4HLhMbrFOH00fhKi-E0d3Vc",
+  authDomain: "notesync-a8772.firebaseapp.com",
+  projectId: "notesync-a8772",
+  storageBucket: "notesync-a8772.firebasestorage.app",
+  messagingSenderId: "253523329549",
+  appId: "1:253523329549:web:2be976ba1d157d3a2f0b44"
+}
+
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+
+export {
+  db
+}
