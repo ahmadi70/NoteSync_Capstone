3daaf4d52cea9d4f56bc3a4c494a2ca15ed4b8b4
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-03-13 20:20:03 +0100

    improve notes page to use progress-bar and default text when no data exist

--- src/pages/PageNotes.vue
+++ src/pages/PageNotes.vue
@@ -21,10 +21,18 @@ const addNote = () => {
       actionBtnLabel="Add New Note"
       placeholder="Add New Note"
     />
-    <Note
-      v-for="note in storeNotes.notes"
-      :key="note.id"
-      :note="note"
-    />
+    <ProgressBar v-if="!storeNotes.notesLoaded"/>
+    <template v-else>
+      <Note
+        v-for="note in storeNotes.notes"
+        :key="note.id"
+        :note="note"
+      />
+    </template>
+    
+    <p
+      v-if="!storeNotes.notes.length"
+      class="text-center text-gray-400 text-2xl pt-4"
+    >No notes here yet...</p>
   </MainSection>
 </template>
