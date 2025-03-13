291bc59c7ac7e7d012c4bc98664f516d826befc7
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-03-13 23:38:55 +0100

    define rout for auth page and use navigation guards

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import PageNotes from '@/pages/PageNotes.vue'
 import PageOverview from '@/pages/PageOverview.vue'
 import PageEditNote from '@/pages/PageEditNote.vue'
+import PageAuth from '@/pages/PageAuth.vue'
+import { useStoreAuth } from '@/stores/storeAuth'
 
 const routes = [
   {
@@ -18,12 +20,28 @@ const routes = [
     path: '/edit-note/:id',
     name: 'edit-note',
     component: PageEditNote
+  },
+  {
+    path: '/auth',
+    name: 'auth',
+    component: PageAuth
   }
 ]
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes
+})
+
+// navigation guards - this will triger every time when a user try to go to a new rout
+router.beforeEach(async (to, from) => {
+  const storeNotes = useStoreAuth()
+  if(!storeNotes.user.id && to.name !== 'auth') {
+    return { name: 'auth' }
+  }
+  if(storeNotes.user.id && to.name === 'auth') {
+    return false
+  }
 })
 
 export default router
